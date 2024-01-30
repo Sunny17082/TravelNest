4f7219d9f75e873794ea8ca7fabe85a22d5fe7a0
Sunny17082 <sunnyprasad1708@gmail.com>
2024-01-30 19:36:16 +0530

    Added register feature

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const bcrypt = require("bcrypt");
+const User = require("./models/user");
 const app = express();
+
+const PORT = process.env.PORT;
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+app.use(express.json());
 
 app.use(
 	cors({
@@ -9,10 +19,24 @@ app.use(
 	})
 );
 
-app.get("/test", (req, res) => {
-    res.json("test ok");
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Connection successful..."))
+    .catch((err) => console.log(err));
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt)
+        });
+        res.json(userDoc);
+    } catch (err) {
+        res.json(err);
+    }
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log("Server started on port 5000...");
-})
+});
