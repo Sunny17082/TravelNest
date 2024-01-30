77073c62636b40189dcf86373ad5afa84986e0d7
Sunny17082 <sunnyprasad1708@gmail.com>
2024-01-30 20:13:27 +0530

    Added find by email in mongodb

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,22 +1,55 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	async function loginUser(e) {
+		e.preventDefault();
+		try {
+			await axios.post("/login", { email, password });
+			alert("Login successful.");
+		} catch (err) {
+			alert("Login failed.");
+		}
+	}
+
     return (
 		<div className="mt-5 grow flex items-center justify-around">
 			<div className="mb-64">
-				<h1 className="text-4xl font-semibold text-center mb-5">Login</h1>
-				<form className="max-w-md mx-auto">
-					<input type="email" placeholder="your@email.com" />
-					<input type="password" placeholder="password" />
+				<h1 className="text-4xl font-semibold text-center mb-5">
+					Login
+				</h1>
+				<form className="max-w-md mx-auto" onSubmit={loginUser}>
+					<input
+						type="email"
+						placeholder="your@email.com"
+						value={email}
+						onChange={(ev) => {
+							setEmail(ev.target.value);
+						}}
+					/>
+					<input
+						type="password"
+						placeholder="password"
+						value={password}
+						onChange={(ev) => {
+							setPassword(ev.target.value);
+						}}
+					/>
 					<button className="primary">Login</button>
 				</form>
 				<div className="text-center py-2 text-gray-500">
-					Don't have an account yet? <Link to="/register" className='underline text-black'>Register now</Link>
+					Don't have an account yet?{" "}
+					<Link to="/register" className="underline text-black">
+						Register now
+					</Link>
 				</div>
 			</div>
 		</div>
 	);
 }
 
-export default LoginPage
+export default LoginPage;
