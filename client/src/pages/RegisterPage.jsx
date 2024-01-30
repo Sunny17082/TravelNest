5056e80b3af5c10fc3f6df41653704ff8e0775b0
Sunny17082 <sunnyprasad1708@gmail.com>
2024-01-30 18:00:07 +0530

    Setup backend api

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
-  return (
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	function registerUser(e) {
+		e.preventDefault();
+	}
+
+  	return (
 		<div className="mt-5 grow flex items-center justify-around">
 			<div className="mb-64">
-				<h1 className="text-4xl font-semibold text-center mb-5">Register</h1>
-				<form className="max-w-md mx-auto">
-					<input type="text" placeholder="your name here" />
-					<input type="email" placeholder="your@email.com" />
-					<input type="password" placeholder="password" />
+				<h1 className="text-4xl font-semibold text-center mb-5">
+					Register
+				</h1>
+				<form className="max-w-md mx-auto" onSubmit={registerUser}>
+					<input
+						type="text"
+						placeholder="your name here"
+						value={name}
+						onChange={(ev) => {
+							setName(ev.target.value);
+						}}
+					/>
+					<input
+						type="email"
+						placeholder="your@email.com"
+						value={email}
+						onChange={(ev) => {
+							setEmail(ev.target.value);
+						}}
+					/>
+					<input
+						type="password"
+						placeholder="password"
+						value={password}
+						onChange={(ev) => {
+							setPassword(ev.target.value);
+						}}
+					/>
 					<button className="primary">Register</button>
 				</form>
 				<div className="text-center py-2 text-gray-500">
-					Already have an account? <Link to="/login" className="underline text-black">login</Link>
+					Already have an account?{" "}
+					<Link to="/login" className="underline text-black">
+						login
+					</Link>
 				</div>
 			</div>
 		</div>
-  );
+	);
 }
 
 export default RegisterPage
