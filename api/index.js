448beb286d3b6a81f5f8fbe42e492138f3e53036
Sunny17082 <sunnyprasad1708@gmail.com>
2024-01-31 14:29:08 +0530

    Updated profile data on reload

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv").config();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("./models/user");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 const PORT = process.env.PORT;
@@ -16,6 +17,9 @@ const secret = process.env.SECRET;
 
 // parse incoming requests with JSON payloads
 app.use(express.json());
+
+// parses cookies attached to the client's request and makes them available in the req.cookies object.
+app.use(cookieParser());
 
 //  It allows requests from http://localhost:5173 to access the server, including credentials in the requests
 app.use(
@@ -51,27 +55,41 @@ app.post("/login", async (req, res) => {
         email
     });
     if (userDoc) {
-		// synchronously compare a plaintext password with a stored hashed password
-		const passOk = bcrypt.compareSync(password, userDoc.password);
+        // synchronously compare a plaintext password with a stored hashed password
+        const passOk = bcrypt.compareSync(password, userDoc.password);
         if (passOk) {
-			// generate a JWT and then setting it as a cookie in a response.
-			jwt.sign(
-				{ email: userDoc.email, id: userDoc._id }, // Payload
-				secret, // Secret key for signing the token
-				{}, // Options
-				(err, token) => {
-					// Callback function
-					if (err) throw err;
-					res.cookie("token", token).json(userDoc);
-				}
-			);
-		} else {
-			res.status(422).json("Password not ok");
-		}
-	} else {
+            // generate a JWT and then setting it as a cookie in a response.
+            jwt.sign(
+                { email: userDoc.email, id: userDoc._id}, // Payload
+                secret, // Secret key for signing the token
+                {}, // Options
+                (err, token) => {
+                    // Callback function
+                    if (err) throw err;
+                    res.cookie("token", token).json(userDoc);
+                }
+            );
+        } else {
+            res.status(422).json("Password not ok");
+        }
+    } else {
         res.status(404).json("not found");
     }
-})
+});
+
+app.get("/profile", (req, res) => {
+    const { token } = req.cookies;
+    if (token) {
+		// to verify the token and send user data to client
+		jwt.verify(token, secret, {}, async (err, userData) => {
+			if (err) throw err;
+			const { name, email, _id } = await User.findById(userData.id);
+			res.json({ name, email, _id });
+		});
+	} else {
+        res.json(null);
+    }
+});
 
 app.listen(PORT, () => {
     console.log("Server started on port 5000...");
