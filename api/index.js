b548bb7afdb42df673d14b67463e926940e64fae
Sunny17082 <sunnyprasad1708@gmail.com>
2024-10-19 21:22:49 +0530

    changed url

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
 	cors({
 		credentials: true,
-		origin: "http://localhost:5173",
+		origin: process.env.FRONTEND_URL,
 	})
 );
 
